1b351f0b6d07e2a893613cf92e72168d8e8d0fb1
thinthread <sjboyette@gmail.com>
2016-08-16 21:51:52 -0700

    Add files via upload

--- parsing_2-13.py
+++ parsing_2-13.py
@@ -10,42 +10,41 @@
 # fish_list_split=fish_list.split("fish")
 # print fish_list_split
 
-str= "item:apples,quantity:4,price:1.50\n"
+string= "item:apples,quantity:4,price:1.50\n"
 
-split_string=str.split(",")
-print split_string
+items = ["item:elephants,quantity:4,price:10000.50\n","item:apples,quantity:4,price:1.50\n","item:pears,quantity:5,price:2.00\n","item:cereal,quantity:1,price:4.49\n"]
 
-split_string2= split_string[2].split(":")
-print split_string2
+def calculate_grocery_bill(string):
+	split_string=string.split(",")
 
-split_string3=split_string2[1].split(",")
-print split_string3
+	split_string2= split_string[2].split(":")
 
-price=float(split_string3[0])
-print price 
+	split_string3=split_string2[1].split(",")
 
-price1=price=float(split_string3[0])
-print price1
+	price=float(split_string3[0])
 
-split_string4=split_string[1].split(":")
-print split_string4
+	price1=price=float(split_string3[0])
 
-split_string5=split_string4[1].split(",")
-print split_string5
+	split_string4=split_string[1].split(":")
 
-quantity=int(split_string5[0])
-print quantity
+	split_string5=split_string4[1].split(",")
 
-quantity1=quantity=int(split_string5[0])
-print quantity1
+	quantity=int(split_string5[0])
 
-def calculate_grocery_bill(total):
+	quantity1=quantity=int(split_string5[0])
+
+
 	total_bill=price1*quantity1
+
+	return total_bill
+
+def main(): 
+	total_bill=0
+	for string in items:
+		total_bill = total_bill + calculate_grocery_bill(string)
 
 	print total_bill
 
-	# calculate_grocery_bill(total_bill)
-print calculate_grocery_bill
-
+main()		
 
 
