7ac8a55c346edede74c66a77a3ade57e9e3d01f5
thinthread <sjboyette@gmail.com>
2016-08-25 21:28:09 -0700

    Add files via upload

---
+++ code_app.py
@@ -0,0 +1,26 @@
+contacts={}
+steph = {"first name": "Stephanie", 'last name': "Boyette" , 
+		"phone": "415-222-3333"}
+jen = {"first name": "Jennifer", "last name": "Kim", 
+	    "phone": "510-333-9999"}
+contacts["steph"] = steph
+contacts["jen"] = jen
+
+# jen["phone"] = "888-888-8888"
+
+# steph["last name"] = "Williams"
+
+def change_number():
+	
+
+def change last_name():
+
+
+def change first_name():
+
+
+def main():
+
+if __name__ == '__main__':
+	main()
+# print contacts
